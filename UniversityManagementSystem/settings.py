cab7c2517ae777ca1112e3fe103e1e9dd5a776b2
coderj001 <amirajubolchi001@gmail.com>
2020-06-28 19:28:48 +0530

    whitenoise

--- UniversityManagementSystem/settings.py
+++ UniversityManagementSystem/settings.py
@@ -129,7 +129,7 @@ EMAIL_FILE_PATH=os.path.join(BASE_DIR,"sent_mails")
 #
 
 #Enable Only Making Project Live on Heroku
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
-import dj_database_url
-prod_db=dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(prod_db)
+# STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+# import dj_database_url
+# prod_db=dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(prod_db)
