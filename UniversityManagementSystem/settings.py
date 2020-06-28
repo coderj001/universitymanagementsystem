64908c33aeae2a4c20e5065db8eef52e980755c0
coderj001 <amirajubolchi001@gmail.com>
2020-06-28 19:53:04 +0530

    update for staticfile

--- UniversityManagementSystem/settings.py
+++ UniversityManagementSystem/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR,'static'),
     )
 #Enable Only Making Project Live on Heroku
-STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 # import dj_database_url
 # prod_db=dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
